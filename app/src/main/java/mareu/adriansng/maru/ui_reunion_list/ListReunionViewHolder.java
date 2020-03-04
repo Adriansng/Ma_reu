d920963e4067412671ab9f65799b7af79bbc4c32
Adrian <senegas.adrian@gmail.com>
2020-03-04 18:20:08 +0200

    Commit
    New RecyclerView
    Spinner Custom

--- app/src/main/java/mareu/adriansng/maru/ui_reunion_list/ListReunionViewHolder.java
+++ app/src/main/java/mareu/adriansng/maru/ui_reunion_list/ListReunionViewHolder.java
@@ -19,39 +19,28 @@ import mareu.adriansng.maru.model.MeetingRoom;
 import mareu.adriansng.maru.model.Reunion;
 
 class ListReunionViewHolder extends RecyclerView.ViewHolder{
-
-    private List<Reunion> mReunions;
+    @BindView(R.id.item_list_reunion_number)
+    TextView mReunionMeetingRoom;
+    @BindView(R.id.item_list_reunion_hour)
+    TextView mReunionHourDate;
+    @BindView(R.id.item_list_reunion_name_organizer)
+    TextView mReunionOrganizer;
+    @BindView(R.id.item_list_reunion_mail)
+    TextView mReunionListMail;
+    @BindView(R.id.item_list_user_delete_button)
+    ImageButton mDeleteButton;
 
     public ListReunionViewHolder(View itemView) {
         super(itemView);
         ButterKnife.bind(this,itemView);
     }
 
-    public void bind( ViewHolder holder, int position) {
-        Reunion mReunion= mReunions.get(position);
+    public void bind( Reunion mReunion) {
        // mReunionMeetingRoom.setText(mReunion.getIdMeetingRoom());
         //mReunionHourDate.setText(mReunion.getHourDate());
         //mReunionOrganizer.setText(mReunion.getNameOrganizer());
        // mReunionListMail.setText((CharSequence) mReunion.getAddressMailList());
-        holder.mDeleteButton.setOnClickListener(v ->
-                EventBus.getDefault().post(new DeleteReunionEvent(mReunion)));
-    }
-
-    public class ViewHolder extends RecyclerView.ViewHolder{
-        @BindView(R.id.item_list_reunion_number)
-        TextView mReunionMeetingRoom;
-        @BindView(R.id.item_list_reunion_hour)
-        TextView mReunionHourDate;
-        @BindView(R.id.item_list_reunion_name_organizer)
-        TextView mReunionOrganizer;
-        @BindView(R.id.item_list_reunion_mail)
-        TextView mReunionListMail;
-        @BindView(R.id.item_list_user_delete_button)
-        ImageButton mDeleteButton;
-
-        ViewHolder(View view){
-            super(view);
-            ButterKnife.bind(this,view);
-        }
+        //mDeleteButton.setOnClickListener(v ->
+                //EventBus.getDefault().post(new DeleteReunionEvent(mReunion)));
     }
 }
