92387dd31fed74db215f1600c6675d1a05c833ba
Adrian <senegas.adrian@gmail.com>
2020-03-04 18:11:22 +0200

    Commit
    New RecyclerView
    Spinner Custom

--- app/src/main/java/mareu/adriansng/maru/ui_reunion_list/ListReunionViewHolder.java
+++ app/src/main/java/mareu/adriansng/maru/ui_reunion_list/ListReunionViewHolder.java
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import org.greenrobot.eventbus.EventBus;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import mareu.adriansng.maru.R;
@@ -18,29 +20,38 @@ import mareu.adriansng.maru.model.Reunion;
 
 class ListReunionViewHolder extends RecyclerView.ViewHolder{
 
-
-    @BindView(R.id.item_list_reunion_number)
-    TextView mReunionMeetingRoom;
-    @BindView(R.id.item_list_reunion_hour)
-    TextView mReunionHourDate;
-    @BindView(R.id.item_list_reunion_name_organizer)
-    TextView mReunionOrganizer;
-    @BindView(R.id.item_list_reunion_mail)
-    TextView mReunionListMail;
-    @BindView(R.id.item_list_user_delete_button)
-    ImageButton mDeleteButton;
+    private List<Reunion> mReunions;
 
     public ListReunionViewHolder(View itemView) {
         super(itemView);
         ButterKnife.bind(this,itemView);
     }
 
-    public void bind(Reunion mReunion) {
+    public void bind( ViewHolder holder, int position) {
+        Reunion mReunion= mReunions.get(position);
        // mReunionMeetingRoom.setText(mReunion.getIdMeetingRoom());
         //mReunionHourDate.setText(mReunion.getHourDate());
         //mReunionOrganizer.setText(mReunion.getNameOrganizer());
        // mReunionListMail.setText((CharSequence) mReunion.getAddressMailList());
-        mDeleteButton.setOnClickListener(v ->
+        holder.mDeleteButton.setOnClickListener(v ->
                 EventBus.getDefault().post(new DeleteReunionEvent(mReunion)));
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.item_list_reunion_number)
+        TextView mReunionMeetingRoom;
+        @BindView(R.id.item_list_reunion_hour)
+        TextView mReunionHourDate;
+        @BindView(R.id.item_list_reunion_name_organizer)
+        TextView mReunionOrganizer;
+        @BindView(R.id.item_list_reunion_mail)
+        TextView mReunionListMail;
+        @BindView(R.id.item_list_user_delete_button)
+        ImageButton mDeleteButton;
+
+        ViewHolder(View view){
+            super(view);
+            ButterKnife.bind(this,view);
+        }
     }
 }
