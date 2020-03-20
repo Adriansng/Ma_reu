fe8a1c6f48dbee927a5f01d5dcdeb50fe036f07f
Adrian <senegas.adrian@gmail.com>
2020-03-20 15:23:29 +0200

    Commit
    RecyclerView Ok
    Spinner Ok
    Delete OK
    Layout/Land Ok

--- app/src/main/java/mareu/adriansng/maru/ui_reunion_list/ListReunionActivity.java
+++ app/src/main/java/mareu/adriansng/maru/ui_reunion_list/ListReunionActivity.java
@@ -1,14 +1,18 @@
 package mareu.adriansng.maru.ui_reunion_list;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -17,16 +21,17 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.ButterKnife;
 import mareu.adriansng.maru.R;
 import mareu.adriansng.maru.di.DI;
 import mareu.adriansng.maru.event.DeleteReunionEvent;
+import mareu.adriansng.maru.model.MeetingRoom;
 import mareu.adriansng.maru.model.Reunion;
 import mareu.adriansng.maru.service_api.ReunionApiService;
-import mareu.adriansng.maru.ui_reunion_list.utils.PopupFilterDate;
-import mareu.adriansng.maru.ui_reunion_list.utils.PopupFilterRoom;
+import mareu.adriansng.maru.ui_reunion_list.utils.SpinnerMeetingRoomAdapter;
 
 public class ListReunionActivity extends AppCompatActivity {
 
@@ -36,10 +41,16 @@ public class ListReunionActivity extends AppCompatActivity {
     private ListReunionActivity activity;
 
     // FOR DATA
-    private ListReunionAdapter adapter;
+
+
     private ReunionApiService reunionApiService;
     private List<Reunion>  mReunions;
 
+    //FOR FILTER
+    private SpinnerMeetingRoomAdapter mAdapterSpinner;
+    private ListReunionAdapter adapter;
+    private ArrayList<MeetingRoom> mMeetingRoom;
+    private MeetingRoom selectionRoom;
 
     // OVERRIDE
 
@@ -95,19 +106,55 @@ public class ListReunionActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.filter_room:
-                final PopupFilterRoom popupFilterRoom= new PopupFilterRoom(activity);
-                popupFilterRoom.build();
-                int selectRoom = popupFilterRoom.getSelectionRoom();
+                AlertDialog.Builder mBuilder= new AlertDialog.Builder(ListReunionActivity.this);
+                View mView= getLayoutInflater().inflate(R.layout.country_spinner_row,null);
+                mBuilder.setTitle("Filter meeting room");
+                Spinner mSpinner= mView.findViewById(R.id.spinner_room_reunion);
+                initListSpinner();
+                mAdapterSpinner= new SpinnerMeetingRoomAdapter(this,mMeetingRoom);
+                mSpinner.setAdapter(mAdapterSpinner);
+                mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        selectionRoom= reunionApiService.getMeetingRoom().get(position);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
+                mBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(mSpinner.getSelectedItem().toString().equalsIgnoreCase("")){
+                            Toast.makeText(ListReunionActivity.this,mSpinner.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                mBuilder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                mBuilder.setView(mView);
+                AlertDialog dialog=mBuilder.create();
+                dialog.show();
                 return (true);
             case R.id.filter_date:
-               final PopupFilterDate popupFilterDate = new PopupFilterDate(activity);
-               DialogFragment datePicker=popupFilterDate.getDatePicker();
-               datePicker.show(getSupportFragmentManager(),"Date picker");
-               popupFilterDate.build();
-               String selectDate= popupFilterDate.getDateSelection();
+               //final PopupFilterDate popupFilterDate = new PopupFilterDate(activity);
+               //DialogFragment datePicker=popupFilterDate.getDatePicker();
+               //datePicker.show(getSupportFragmentManager(),"Date picker");
+               //popupFilterDate.build();
+               //String selectDate= popupFilterDate.getDateSelection();
                return (true);
         }
         return super.onOptionsItemSelected(item);
+    }
+    private void initListSpinner() {
+        mMeetingRoom = new ArrayList<>();
+        mMeetingRoom.addAll(reunionApiService.getMeetingRoom());
     }
 
     @Override
