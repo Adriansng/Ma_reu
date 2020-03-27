178b7373d1da7e4c9b9bd95934a0035c752aa48f
Adrian <senegas.adrian@gmail.com>
2020-03-27 21:00:47 +0300

    Commit
    RecyclerView Ok
    Spinner Ok
    Delete OK
    Layout/Land Ok

--- app/src/main/java/mareu/adriansng/maru/service_api/DummyReunionList.java
+++ app/src/main/java/mareu/adriansng/maru/service_api/DummyReunionList.java
@@ -26,19 +26,19 @@ public abstract class DummyReunionList {
     );
 
     //Generate Person
-    public  List<Person> DUMMY_PERSON= Arrays.asList(
+    public static List<Person> DUMMY_PERSON= Arrays.asList(
             new Person(0,"Pierre","pierre@maReu.com"),
             new Person(1, "Manuel", "manuel@maReu.com"),
             new Person(2, "Bran", "bran@maReu.com")
     );
 
-    public List<Reunion> DUMMY_REUNION = Arrays.asList(
+    public static List<Reunion> DUMMY_REUNION = Arrays.asList(
             new Reunion(0,2, "Henri", "10H00","05/19/2020",Arrays.asList(DUMMY_PERSON.get(0),DUMMY_PERSON.get(1),DUMMY_PERSON.get(2))),
             new Reunion(1, 5, "Bernard", "10H30", "09/02/2020",Arrays.asList(DUMMY_PERSON.get(0),DUMMY_PERSON.get(1),DUMMY_PERSON.get(2))),
             new Reunion(2,8, "Arnaud", "15H00", "01/01/2021",Arrays.asList(DUMMY_PERSON.get(0),DUMMY_PERSON.get(1),DUMMY_PERSON.get(2)))
      );
 
-    List<Reunion> generateReunion() {
+    static List<Reunion> generateReunion() {
         return new ArrayList<>(DUMMY_REUNION);
     }
 }
