73c42d0a1c1b16e8d9179318848d6c6c518ccc25
Adrian <senegas.adrian@gmail.com>
2020-04-06 17:24:54 +0200

    Commit
    RecyclerView Ok
    Spinner Ok
    Delete OK
    Layout/Land Ok

--- app/src/main/java/mareu/adriansng/maru/ui_reunion_list/utils/DateUtils.java
+++ app/src/main/java/mareu/adriansng/maru/ui_reunion_list/utils/DateUtils.java
@@ -1,47 +1,40 @@
 package mareu.adriansng.maru.ui_reunion_list.utils;
 
-import android.text.format.DateFormat;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.TimeZone;
 
 public class DateUtils {
      private static final String TAG= "DateUtils";
 
-
-     public static String getDateTimeFromTimeStamp(Long time, String mDateFormat) {
-         SimpleDateFormat dateFormat= new SimpleDateFormat(mDateFormat);
-         dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-         Date dateTime= new Date(time);
-         return dateFormat.format(dateTime);
-     }
-
-     public static long getTimeStampFromDateTime(String mDateTime, String mDateFormat)
-         throws ParseException{
-         SimpleDateFormat dateFormat= new SimpleDateFormat(mDateFormat);
-         dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-         Date date= dateFormat.parse(mDateTime);
-         return date.getTime();
-     }
-
-     public static String formatDateTimeFromDate(String mDateFormat, Date date) {
-         if (date==null) {
-             return  null;
+     public static String formatDateLong(String dateString){
+         SimpleDateFormat formatter, FORMATTER;
+         Date date= null;
+         String myDate="";
+         formatter= new SimpleDateFormat("MM/jj/aaaa");
+         try{
+             date= formatter.parse(dateString);
+             FORMATTER=new SimpleDateFormat("jj MMMM aaaa");
+             myDate= FORMATTER.format(date);
+         } catch (ParseException e) {
+             e.printStackTrace();
          }
-         return  DateFormat.format(mDateFormat, date).toString();
+         return myDate;
      }
 
-     public static String formatDateFromDateString(String inputDateFormat,String outputDateFormat, String inputDate)
-         throws ParseException{
-         Date mParsedDate;
-         String mOutputDateString;
-         SimpleDateFormat mInputDateFormat= new SimpleDateFormat(inputDateFormat,java.util.Locale.getDefault());
-         SimpleDateFormat mOutputDateFormat= new SimpleDateFormat(outputDateFormat,java.util.Locale.getDefault());
-         mParsedDate= mInputDateFormat.parse(inputDate);
-         mOutputDateString= mOutputDateFormat.format(mParsedDate);
-         return  mOutputDateString;
-         }
+    public static String formatDateVeryLong(String dateString){
+        SimpleDateFormat formatter, FORMATTER;
+        Date date= null;
+        String myDate="";
+        formatter= new SimpleDateFormat("MM/jj/aaaa");
+        try{
+            date= formatter.parse(dateString);
+            FORMATTER=new SimpleDateFormat("jj MMMM aaaa");
+            myDate= FORMATTER.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return myDate;
+    }
 
 }
